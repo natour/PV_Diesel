262505636756dea522a850a922b0ac716bd7ebd2
natour <natour@gmail.com>
2025-06-30 07:23:55 +0400

    Update bess_simulator.py

--- bess_simulator.py
+++ bess_simulator.py
@@ -64,7 +64,7 @@ def simulate_energy_balance(pv_output, load_profile, diesel_l_per_kwh, generator
                     
                     gen_load = min_loading_kw 
                     pv_used[i]=load-gen_load
-                    if pv_used[I]>pv:
+                    if pv_used[i]>pv:
                          pv_used[i]=pv
                          gen_load = load-pv
                     gen_output[i]=gen_load# -pv_used[i]# min_loading_kw
